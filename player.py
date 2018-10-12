76c1eedcd22056d995ed1917476c4b22e711fc0d
jose <nin3377@gmail.com>
2018-10-12 14:43:23 +0200

    Start

---
+++ player.py
@@ -0,0 +1,135 @@
+from flask import Flask
+from flask import jsonify
+import subprocess
+import requests
+import json
+import os,binascii
+from wowzaCloudAPI import .
+
+app = Flask(__name__)
+
+@app.route('player/<id>', methods=['GET'])
+def player(id):
+    if id == 1:
+        cloud_id = 'bx1vkpcc'
+        name = 'Reconciliate con Dios'
+    res = josestreamCloud()
+    res = res.getTarget(cloud_id)
+    token_id = res.getToken(cloud_id)
+    src = res['hls_playback']
+    variable = '--key=' +token_id
+    try:
+        result = subprocess.check_output(['python','akamai_token_v2.py','--window=300', variable,'--acl=/*'])
+    except:
+        result = 'Error!'
+    params = {'src'=src, 'token'=token, 'name'=name}
+    return render_template('player.html', params=params)
+
+@app.route('/token/<token_id>', methods = ['POST'])
+def token(token_id):
+    variable = '--key=' +token_id
+    try:
+        result = subprocess.check_output(['python','akamai_token_v2.py','--window=300', variable,'--acl=/*'])
+    except:
+        result = 'Error!'
+    a = result
+    response = jsonify({'params':a})
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
+@app.route('/player/<player_id>', methods = ['GET'])
+def player(player_id):
+    #try:
+    #    r = requests.get('http://api.josestream.com/v1/stream/'+player_id)
+    #    r = r.json()
+    #    token = r['player_token']
+    #    base_hls = r['player_hls']
+    #except:
+    base_hls = 'https://wowzaprod109-i.akamaihd.net/hls/live/623120/afc62c0c/playlist.m3u8'
+    token = '0d84080361930851d9c0eb66a12cd270'
+    #url='http://localhost:5000/token/' + token
+    #try:
+    #    res = requests.post(url)
+    #    res = res.json()
+    #    token_auth = res['params']
+    #except:
+    variable = '--key=' + token
+    result = subprocess.check_output(['python','akamai_token_v2.py','--window=300', variable,'--acl=/*'])
+    a = result.rstrip()
+    #resp = jsonify({'params':a})
+    #res = resp.json()
+    #token_auth = res['params']
+    poster = 'https://wallpaper.wiki/wp-content/uploads/2017/06/Blue-gradient-Wallpaper-1080p.png'
+    video_url = base_hls + '?hdnts=' + a
+    base_html = '<html><head><script src="http://old.josestream.com/p/player.js"></script><link href="http://old.josestream.com/p/min.css" rel="stylesheet"><link href="https://old.josestream.com/player.css" rel="stylesheet"></head><body><video id="content_video" poster="'+poster+'" class="video-js vjs-default-skin vjs-fluid vjs-16-9"  preload="auto"   title="TestVideo1"></video><script>videojs("content_video", {  controls: true,sources: [{ "type": "application/x-mpegURL", "src": "'+video_url+'"  }],  plugins: {  videoJsResolutionSwitcher: {  default: "240",dynamicLabel: false } } ,html5: { hls: {debug: true, } } }, function(){var player = this;player.on("resolutionchange", function(){console.info("Source changed")})});</script></body></body>'
+    response = base_html
+    return response
+
+@app.route('/stream', methods = ['POST'])
+def stream():
+    '''
+    Define the api request:
+        Auth --> UserName: email & api key
+        Body --> Name for stream & Input resolution
+            - extras: ??
+
+    Key -- SET
+    Transcoder -- SET
+    Target CDN -- NO
+    '''
+    user = request.headers.get('user')
+    key = request.headers.get('key')
+    try:
+        userDB = user.objects.get(user=user)
+        xkey = user.objects.get() # get the password key
+        status = True
+    except:
+        return 'User not in system'
+    if status == True:
+        if xkey == key:
+            server = 'http://live-eu.josestream.com'
+
+            if input_res >= 1080: # Used to set the transcoder resolution!
+                templateName = '1080'
+            elif input_res < 1080 and input_res >= 720:
+                templateName = '720'
+            elif input_res < 720 and input_res >= 576:
+                templateName = '576'
+            elif input_res < 576 and input_res >= 480:
+                templateName = '480'
+            elif input_res < 480 and input_res >= 360:
+                templateName = '360'
+            elif input_res < 360 and input_res >= 0:
+                templateName = '240'
+            else:
+                return jsonify({'error':'invalid input resolution'})
+
+            #create stream key
+            key_ok = False
+            while key_ok == False:
+                key = binascii.b2a_hex(os.urandom(10))
+                #
+                # Check DB for key's
+                #
+                if key in db:
+                    key_ok = False
+                elif key not in db:
+                    key_ok = True
+                    #
+                    # Add key to database, link to account
+                    #
+            # Get from pool db a target.
+            # Move it to link
+            # Add to
+            copy = '/v2/servers/{}/vhosts/{}/transcoder/templates/{}/actions/{}?dstEntryName={}'.format(serverName, vhostName, templateName, action='copy', newCode = '') # Create a transcoder
+
+
+
+            response = { "live_info" : 'test' }
+            response = jsonify(response)
+            response.headers.add('Access-Control-Allow-Origin', '*')
+            return response
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port=5002,debug=True)
---
+++ wowzaCloudAPI.py
@@ -0,0 +1,128 @@
+# Wowza Cloud & Engine REST API v1
+#   for Josestrem 2018
+#   ------------------
+# v0.1 of the python file
+#   ------------------
+
+import json
+import requests
+import xml.etree.ElementTree as ET #For the XML responses
+import math
+
+class josestreamCloud(object):
+    def __init__(self):
+        self.code = ''
+        self.cloudKey = 'r53CQvVmt8GbZYT79ZHzDfMSnpP2NVDEbaXlkqCQjdafpZj5olJ7wlXaiptZ3546'
+        self.cloudAuth = 'ArIUhr5ql2j9QpsU43YMMLUjVnLhIQ6rqcWaMXylPuROYy9X7L2O6jLK3Gdi332f'
+        self.headers = {'wsc-api-key': self.cloudKey,'wsc-access-key': self.cloudAuth,'Content-Type': 'application/json',}
+        self.partURL = 'https://api-sandbox.cloud.wowza.com/api/'
+        self.version = 'v1'
+        self.url = self.partURL + self.version
+
+    def responseStatus(self, res):
+        if res.status_code == 200:
+            print('Success')
+            return True
+        else:
+            print('Error!')
+            print(res.content)
+            return False
+
+    def setData(self, identifier): #NOT USEFULL NOW :)
+        response = requests.get(self.url+'/stream_targets/'+identifier, headers=self.headers)
+        #??????????????????????????????
+
+
+        self.code = '' #GET user stream cloud code (Wowza Cloud) --> something like this --> user.object.get(code)
+
+
+    def newTarget(self, name):
+        data = { "stream_target": { "name": name, "provider": "akamai_cupertino", "type": "WowzaStreamTarget" }}
+        data = json.dumps(data)
+        response = requests.post(self.url+'/stream_targets/', headers=self.headers, data=data)
+        self.responseStatus(response)
+        if True:
+            response = response.json()
+            base = response['stream_target']
+            identifier = base['id']
+            streamCode =  base['stream_name']
+            hls = base['hls_playback_url']
+            connectionCode = base['connection_code']
+            print('ID:{} Stream Name:{} | hls url:{} | Connection Code: {}').format( identifier,streamCode,hls,connectionCode)
+
+
+    def getTarget(self, identifier):
+        response = requests.get(self.url+'/stream_targets/'+identifier, headers=self.headers)
+        self.responseStatus(response)
+
+        response = response.json()
+        base = response['stream_target'] #Base
+        streamCode = base['stream_name']
+        hls = base['hls_playback_url']
+        https = base['use_https']
+        data = {'stream_target'=base, 'stream_name'=streamCode, 'hls_playback'= hls}
+        return(data)
+
+
+     #GeoBlocking --> Requiere sales to active (per change)...so jxc-ni5-mwp
+
+     def getToken(self, identifier):
+         response = requests.get(self.url+'/stream_targets/'+identifier+'/token_auth', headers=self.headers)
+         self.responseStatus(response)
+         response = response.json()
+         return response['token_auth']['trusted_shared_secret']
+
+    def createGeoBlock(self, code, ok):
+        if ok == false:
+            allow = ''
+        response = requests.post(self.url+'/stream_targets/'+code+'/geoblock', headers=self.headers, data=data)
+    #to -- sales, bla bla bla....
+
+    def delTarget(self, identifier):
+        response = request.delete(self.url+'/stream_targets/'+identifier+'/properties/????', headers=self.headers) # To see what the ??? is about !!!
+        response2 = requests.get(self.url+'/stream_targets/'+identifier, headers=self.headers)
+        if response2.status_code == 410:
+            print('Success! - Target Deleted')
+
+
+    #####       Usage Data      #####
+    def getAnalytics(self, code):
+        response = requests.get(self.url+'/usage/viewer_data/stream_targets/'+code, headers=self.headers)
+        #Check
+        self.responseStatus(response)
+        #Parse
+        response = response.json()
+        countryList = response['stream_target']['country_list']
+        i = 0
+        while i < len(countryList):
+            #i = o - 1
+            code = countryList[i]
+            base = response['stream_target']['countries'][i][code]
+            countryName = base['name']
+            percentageViewersC = base['percentage_viewers']
+            percentageViewingT = base['percentage_viewing_time']
+            averageTimeViewing = base['seconds_avg_viewing_time']
+            totalTimeViewing = base['seconds_total_viewing_time']
+            uniqueViews = base['total_unique_viewers']
+            print(('{} {} |Viewers: {}% (Time {}%, Average {}min, Total {}min) |Unique Viewers: {}').format(code, countryName, percentageViewersC, percentageViewingT, math.ceil(averageTimeViewing), math.ceil(totalTimeViewing), uniqueViews))
+            i += 1
+
+    def getnetworkUsage(self):
+        response = requests.get(self.url+'/usage/network/stream_targets', headers=self.headers)
+        #Check
+        self.responseStatus(response)
+        #Parse
+        response = response.json()
+        i = 0
+        totalUsed = response['total']['bytes_used']
+        totalBilled = response['total']['bytes_billed']
+        targets = response['stream_source']
+        print(('Total used: {} | Total billed:{}').format(totalUsed, totalBilled))
+        while i < len(targets):
+            base = response['stream_targets'][i]
+            billed = base['bytes_billed']
+            used = base['bytes_used']
+            deleted = base['deleted']
+            identifier = base['id']
+            name = base['name']
+            print(('{} {} (Deleted: {}) Used:{} | Billed:{}').format(identifier,name,deleted,used,billed))
